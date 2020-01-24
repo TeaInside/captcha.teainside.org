62cc9a036ff98a7d4c06faa39b24e4f2d840fb28
Ammar Faizi <ammarfaizi2@gmail.com>
2020-01-24 21:18:18 +0700

    Clean up

--- isolated/captcha/calculus/calculus_0007.php
+++ isolated/captcha/calculus/calculus_0007.php
@@ -6,34 +6,18 @@
 switch (rand(1, 1)) {
     case 1:
 
+        $cost = rand(-100, 100);
+        $answer = $cost / 2;
+        $div = rand(2, 11);
+        $cost *= $div;
 
-        $answer = rand(-30, 30);
-        $expt = $answer ** 3;
+        $epx = [
+            "\\left(\\sum_{n = 0}^{\\infty} \\frac{x^{n}}{\\Gamma(n + 1)}\\right)"
+        ][rand(0, 0)];
 
-        $costA = 3628800;
-        $costB = 40320;
-        $fcA = $costA - $expt;
-        $fcB = $costB - $expt;
+        $latex = "\\frac{".$cost."}{".$div."} \\int_{0}^{1} \\left( ".$epx." \\frac{\\sin(x)}{e(\\sin(1) - \\cos(1)) + 1} \\right) dx";
 
-        $latex = <<<LATEX
-\\sqrt[3]{\\ln\\left(
-\\sum_{a = 1}^{\\infty} 
-  \\left(
-    \\int_{0}^{\\infty} \\frac{x^{a - 1}
-            \\exp\\left(-x\\left(\\Gamma(11) - {$fcA}\\right)^{-1}\\right)
-     }{
-         \\left(\\Gamma(8) - {$fcB}\\right)
-         \\left((a - 1)!\\right)^{2}
-    }\\;dx
-  \\right)
-\\right)}
-LATEX;
-
-        $latex = str_replace(["\n", " "], "", $latex);
-
-		$msg = "Evaluate this expression!";
         break;
-    
     default:
         break;
 }
