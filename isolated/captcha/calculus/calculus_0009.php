7c644b72bd8b209cbae1e0d244c4bd09aeef4cae
Ammar Faizi <ammarfaizi2@gmail.com>
2020-05-01 20:31:03 +0700

    Add new captcha

--- isolated/captcha/calculus/calculus_0009.php
+++ isolated/captcha/calculus/calculus_0009.php
@@ -7,13 +7,20 @@ function factorial($n) {
     return $n ? $n : 1;
 }
 
-switch (rand(0, 0)) {
+switch (rand(1, 1)) {
     case 0:
         $a = rand(4, 10);
         $p = rand(1, 2000);
         $answer = $p + (factorial($a)/4);
         $latex = ' & \text{Calculate } \\\\
  & '.$p.' + \int\limits_{0}^{1} \frac{\ln^2(1 + x) \Gamma('.($a + 1).')}{x \operatorname{Li}_3(1)} dx';
+        break;
+    case 1:
+        // Ref: https://math.stackexchange.com/questions/402937/proof-of-sum-n-1-infty-frac1n4-binom2nn-frac17-pi43240
+        $a = rand(2, 100);
+        $answer = $a * 17;
+        $latex = ' & \text{Calculate } \\\\
+ & \frac{'.($a * 36).'}{\zeta(4)}\sum_{n = 1}^{\infty} \frac{1}{n^4 \binom{2n}{n}}';
         break;
 }
 
